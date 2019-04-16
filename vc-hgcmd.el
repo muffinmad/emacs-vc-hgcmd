a0c25ca19ea516be65cf9ed3626cb34ab766fd1c
muffinmad <andreyk.mad@gmail.com>
2019-04-16 22:38:29 +0300

    Use relative filename on rename

--- vc-hgcmd.el
+++ vc-hgcmd.el
@@ -5,7 +5,7 @@
 ;; Author: Andrii Kolomoiets <andreyk.mad@gmail.com>
 ;; Keywords: vc
 ;; URL: https://github.com/muffinmad/emacs-vc-hgcmd
-;; Package-Version: 1.5.1
+;; Package-Version: 1.5.2
 ;; Package-Requires: ((emacs "25.1"))
 
 ;; This file is NOT part of GNU Emacs.
@@ -1209,7 +1209,7 @@ Insert output to process buffer and check if amount of data is enought to parse 
 
 (defun vc-hgcmd-rename-file (old new)
   "Rename file from OLD to NEW using `hg mv'."
-  (vc-hgcmd-command "move" old new))
+  (vc-hgcmd-command "move" (vc-hgcmd--file-relative-name old) (vc-hgcmd--file-relative-name new)))
 
 (defun vc-hgcmd--file-unresolved-p (file)
   "Return t if FILE is in conflict state."
